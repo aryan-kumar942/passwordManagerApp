7b9fda08bdf472ba2ba238e707e6b58ecc6ecf6e
aryan-kumar942 <aryan7067718303@gmail.com>
2026-01-08 10:35:10 +0530

    Initial Commit

---
+++ PasswordManagerApp/PasswordManagerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerAppApp.swift
+//  PasswordManagerApp
+//
+//  Created by Aryan kumar giri on 08/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManagerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PasswordManagerAppTests/PasswordManagerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerAppTests.swift
+//  PasswordManagerAppTests
+//
+//  Created by Aryan kumar giri on 08/01/26.
+//
+
+import Testing
+@testable import PasswordManagerApp
+
+struct PasswordManagerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
